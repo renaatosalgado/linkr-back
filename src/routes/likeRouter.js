61d092474afbc3fbd3a59e0c4182399518d22d7b
rodriguesss <viniciusrdefreitas@gmail.com>
2022-03-23 15:41:19 -0300

    removing import in like router

--- src/routes/likeRouter.js
+++ src/routes/likeRouter.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { validateToken } from "../middleware/validateToken.js";
 import { checkLikeUser, toogleLike, totalLike } from "../controllers/likesController.js";
 
 const likeRouter = Router();
