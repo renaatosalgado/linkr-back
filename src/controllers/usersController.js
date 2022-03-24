0e7ae1325305358a356a3460a09b8fbf4f98860b
renaatosalgado <renaato.salgado@gmail.com>
2022-03-24 15:48:50 -0300

    feat: search bar

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,22 +1,36 @@
-import { userRepository } from '../repositories/userRepository.js';
-import bcrypt from 'bcrypt';
+import { userRepository } from "../repositories/userRepository.js";
+import bcrypt from "bcrypt";
 
 export async function createUser(req, res) {
-    const newUser = req.body;
-    const passwordHash = bcrypt.hashSync(newUser.password, 10);
+  const newUser = req.body;
+  const passwordHash = bcrypt.hashSync(newUser.password, 10);
 
-    try {
-        const user = await userRepository.getUserByEmail(newUser.email);
+  try {
+    const user = await userRepository.getUserByEmail(newUser.email);
 
-        if (user.rowCount !== 0) {
-            return res.sendStatus(409);
-        }
-
-        await userRepository.createUser({ ...newUser, passwordHash });
-
-        return res.sendStatus(201);
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(500);
+    if (user.rowCount !== 0) {
+      return res.sendStatus(409);
     }
+
+    await userRepository.createUser({ ...newUser, passwordHash });
+
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function searchUser(req, res) {
+  const { name } = req.query;
+  if (!name) return;
+
+  try {
+    const result = await userRepository.getUserByName(name);
+    console.log(result);
+
+    res.send(result.rows);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
