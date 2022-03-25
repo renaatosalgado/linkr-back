89550764a30ca1fd88f07787f33c6e6996ca8770
renaatosalgado <renaato.salgado@gmail.com>
2022-03-25 14:27:14 -0300

    fix: merge

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,6 @@
 import { postsRepository } from "../repositories/postsRepository.js";
 import urlMetadata from "url-metadata";
+import { userRepository } from "../repositories/userRepository.js";
 
 export async function createPost(req, res) {
   const { url, description } = req.body;
@@ -38,6 +39,46 @@ export async function createPost(req, res) {
 export async function listPosts(req, res) {
   try {
     const result = await postsRepository.listAll();
+
+    res.status(200).send(result.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
+
+export async function listUserPosts(req, res) {
+  const { id: userId } = req.params;
+  try {
+    const { rows: posts } = await postsRepository.userPosts(userId);
+    const {
+      rows: [user],
+    } = await userRepository.getUserById(userId);
+    const authorName = user.name;
+
+    res.status(200).send({ posts: [...posts], authorName });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
+
+export async function editPost(req, res) {
+  const { id } = req.params;
+  const { description } = req.body;
+  try {
+    await postsRepository.editPost(description, id);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
+
+export async function getHashtagPost(req, res) {
+  const { hashtag } = req.params;
+  try {
+    const result = await postsRepository.listHashtag(hashtag);
 
     res.status(200).send(result.rows);
   } catch (error) {
