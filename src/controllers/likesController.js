9754a09a6ec2e34f7860a9242d5732012954f32e
rodriguesss <viniciusrdefreitas@gmail.com>
2022-03-24 22:16:58 -0300

    fixed bugs in like repository, like controller and like router

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -1,12 +1,13 @@
 import { userRepository } from "../repositories/userRepository.js";
 import { likeRepositoy } from "../repositories/likeRepositoy.js";
 
-export async function toogleLike(req, res) {
+export async function toggleLike(req, res) {
+  const { like, postId } = req.body;
+
   try {
-    const { like, postId } = req.body;
     const { user: userLocals } = res.locals;
 
-    const { rows: userRows } = await userRepository.getUserById(userLocals.id);
+    const { rows: userRows } = await userRepository.getUserById(userLocals?.id);
 
     if (userRows.length === 0) {
       return res.sendStatus(404).send('User not found');
@@ -15,9 +16,9 @@ export async function toogleLike(req, res) {
     const [user] = userRows;
 
     if (!like) {
-      await likeRepositoy.insertLike(user.id, postId);
+      await likeRepositoy.insertLike(user?.id, postId);
     } else {
-      await likeRepositoy.removeLike(user.id, postId);
+      await likeRepositoy.removeLike(user?.id, postId);
     }
 
     return res.status(200).send(!req.body.like);
@@ -28,8 +29,9 @@ export async function toogleLike(req, res) {
 }
 
 export async function totalLike(req, res) {
+  const { postId } = req.params;
+
   try {
-    const { postId } = req.params;
     const { rows } = await likeRepositoy.totalLike(postId);
 
     if (rows.length === 0) {
@@ -44,11 +46,12 @@ export async function totalLike(req, res) {
 }
 
 export async function checkLikeUser(req, res) {
+  const { postId } = req.params;
+
   try {
-    const { postId } = req.params;
     const { user: userLocals } = res.locals;
 
-    const { rows: userRows } = await userRepository.getUserById(userLocals.id);
+    const { rows: userRows } = await userRepository.getUserById(userLocals?.id);
 
     if (userRows.length === 0) {
       return res.sendStatus(404).send('User not found');
@@ -56,13 +59,41 @@ export async function checkLikeUser(req, res) {
 
     const [user] = userRows;
 
-    const { rows: likeRows } = await likeRepositoy.checkLike(user.id, postId);
+    const { rows: likeRows } = await likeRepositoy.checkLike(user?.id, postId);
 
     if (likeRows.length === 0) {
       return res.status(200).send(false)
-    } else {
-      return res.status(200).send(true);
     }
+
+    return res.status(200).send(true);
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+}
+
+export async function getTwoNamesThatLiked(req, res) {
+  const { postId } = req.params;
+  const { user: userLocals } = res.locals;
+
+  try {
+    const { rows: checkLikeRows } = await likeRepositoy.checkLike(userLocals?.id, postId);
+    let userLocalsLike = false;
+
+    if (checkLikeRows.length > 0) {
+      userLocalsLike = true;
+    }
+
+    const { rows: likeRows } = await likeRepositoy.getTwoNamesThatLiked(userLocals?.id, postId);
+
+    const { rows: totalLikes } = await likeRepositoy.totalLike(postId);
+
+    const [likes] = totalLikes;
+
+    likeRows.push({ userLiked: userLocalsLike, likes });
+
+    res.status(200).send(likeRows);
   } catch (error) {
     console.log(error);
     return res.status(500).send(error);
