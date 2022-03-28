5f80083db1aba22e300e744c50bade6da6d63861
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-03-28 09:59:54 -0300

    refactor: add string sql to new functions in repositories

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -11,16 +11,13 @@ async function publish(
     urlDescription,
     urlImage
 ) {
-    return connection.query(
-        `
-    INSERT INTO posts (description, url, "userId", "urlTitle", "urlDescription", "urlImage") VALUES ($1, $2, $3, $4, $5, $6) RETURNING id
-    `,[description, url, userId, urlTitle, urlDescription, urlImage])
-    // const query = format(
-    //     `INSERT INTO posts (description, url, "userId", "urlTitle", "urlDescription", "urlImage") 
-    //     VALUES (?, ?, ?, ?, ?, ?)`,
-    //     [description, url, userId, urlTitle, urlDescription, urlImage]
-    // );
-    // return connection.query(query);
+    const query = format(
+        `INSERT INTO posts (description, url, "userId", "urlTitle", "urlDescription", "urlImage") 
+         VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
+        [description, url, userId, urlTitle, urlDescription, urlImage]
+    );
+
+    return connection.query(query);
 }
 
 async function listAll() {
@@ -38,63 +35,45 @@ async function listAll() {
 }
 
 async function listHashtag(hashtag) {
-  return connection.query(`
-  SELECT p.*, 
-  u.name author, u.image "profilePicture" 
-  FROM posts p
-  LEFT JOIN users u ON u.id = p."userId"
-  WHERE p.description LIKE $1
-  ORDER BY p.id 
-  DESC
-  LIMIT 20
-  `,[`% #${hashtag} %`]);
+    const query = format(
+        `SELECT p.*, 
+        u.name author, u.image "profilePicture" 
+        FROM posts p
+        LEFT JOIN users u ON u.id = p."userId"
+        WHERE p.description LIKE $1
+        ORDER BY p.id 
+        DESC
+        LIMIT 20`,
+        [`% #${hashtag} %`]
+    );
+
+    return connection.query(query);
 }
 
+async function editPost(description, id, hashtags) {
+    const queryDelete = format(`DELETE FROM "postsTrends" WHERE "postId"=?`, [
+        id,
+    ]);
+    const queryUpdate = format(
+        `UPDATE posts
+        SET description = $1 
+        WHERE id=$2`,
+        [description, id]
+    );
 
+    await connection.query(queryDelete);
 
-async function editPost(description, id,hashtags) {
-  await connection.query(`DELETE FROM "postsTrends" WHERE "postId"=$1`,[id])
+    verifyHashtags(hashtags, id);
 
-  verifyHashtags(hashtags,id)
-  return connection.query(
-  `
-  UPDATE posts
-  SET description = $1 
-  WHERE id=$2
-  `,
-    [description, id]
-  );
-    // const query = format(
-    //     `SELECT p.*, 
-    //     u.name author, u.image "profilePicture" 
-    //     FROM posts p
-    //     LEFT JOIN users u ON u.id = p."userId"
-    //     WHERE p.description LIKE ?
-    //     ORDER BY p.id 
-    //     DESC
-    //     LIMIT 20`,
-    //     [`%#${hashtag}%`]
-    // );
-    // return connection.query(query);
+    return connection.query(queryUpdate);
 }
 
-// async function editPost(description, id) {
-//     const query = format(
-//         `UPDATE posts
-//         SET description = ?
-//         WHERE id=?`,
-//         [description, id]
-//     );
-//     return connection.query(query);
-// }
-
-async function addHashtagsPost(hashtags,postId){
-  console.log(postId);
-  try{
-    verifyHashtags(hashtags,postId)
-  }catch(error){
-    console.log(error)
-  }   
+async function addHashtagsPost(hashtags, postId) {
+    try {
+        verifyHashtags(hashtags, postId);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function userPosts(userId) {
@@ -113,33 +92,48 @@ async function userPosts(userId) {
     return connection.query(query);
 }
 
-async function verifyHashtags(hashtags,postId){
-  const trends = await connection.query(`SELECT * FROM trends`);
-    
-  for(let i = 0; i < hashtags.length; i++){
-    for(let j = 0; j < trends.rows.length; j++){
-      if(hashtags[i] === trends.rows[j].name){
-        await connection.query(`INSERT INTO "postsTrends" ("trendId","postId") VALUES ($1,$2)`, [trends.rows[j].id,postId])
-        break;
-      }
-      if(j === trends.rows.length-1){
-        const hashtagId = await connection.query(`INSERT INTO trends (name) VALUES ($1) RETURNING id`,[hashtags[i]])
-        await connection.query(`INSERT INTO "postsTrends" ("trendId","postId") VALUES ($1,$2)`, [hashtagId.rows[0].id,postId])
-      }
+async function verifyHashtags(hashtags, postId) {
+    const trends = await connection.query(format(`SELECT * FROM trends`));
+
+    for (let i = 0; i < hashtags.length; i++) {
+        for (let j = 0; j < trends.rows.length; j++) {
+            if (hashtags[i] === trends.rows[j].name) {
+                await connection.query(
+                    format(
+                        `INSERT INTO "postsTrends" ("trendId","postId") VALUES (?,?)`,
+                        [trends.rows[j].id, postId]
+                    )
+                );
+                break;
+            }
+            if (j === trends.rows.length - 1) {
+                const hashtagId = await connection.query(
+                    format(
+                        `INSERT INTO trends (name) VALUES ($1) RETURNING id`,
+                        [hashtags[i]]
+                    )
+                );
+                await connection.query(
+                    format(
+                        `INSERT INTO "postsTrends" ("trendId","postId") VALUES ($1,$2)`,
+                        [hashtagId.rows[0].id, postId]
+                    )
+                );
+            }
+        }
     }
-  }
 }
 
 async function deletePost(postId) {
-  await connection.query(`DELETE FROM "postsTrends" WHERE "postId"=$1`,[postId])
-    const query = format(
-        `DELETE 
-        FROM posts
-        WHERE id=?`,
+    const queryPostsTrends = format(
+        `DELETE FROM "postsTrends" WHERE "postId"=?`,
         [postId]
     );
+    const queryPosts = format(`DELETE FROM posts WHERE id=?`, [postId]);
 
-    return connection.query(query);
+    await connection.query(queryPostsTrends);
+
+    return connection.query(queryPosts);
 }
 
 export const postsRepository = {
--- src/repositories/trendsRepository.js
+++ src/repositories/trendsRepository.js
@@ -1,15 +1,19 @@
 import connection from '../db.js';
+import pkg from 'sqlstring';
 
-async function trends(){
-    return connection.query(`
-        SELECT t.name, COUNT(pt."trendId") AS total FROM trends t
+const { format } = pkg;
+
+async function trends() {
+    const query = format(
+        `SELECT t.name, COUNT(pt."trendId") AS total 
+        FROM trends t
         JOIN "postsTrends" pt ON t.id = pt."trendId"
         GROUP BY t.name
         ORDER BY total DESC
-        LIMIT 10
-        `
-    )
+        LIMIT 10`
+    );
 
+    return connection.query(query);
 }
 
-export const trendsRepository = {trends}
+export const trendsRepository = { trends };
