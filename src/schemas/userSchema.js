5f70440b5a7113dddce2d8bc73f37241179e57a1
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-03-29 21:24:49 -0300

    fix: changing username schema

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -3,8 +3,8 @@ import joi from 'joi';
 const userSchema = joi.object({
     email: joi.string().email().required(),
     password: joi.string().required(),
-    username: joi.string().required(),
-    picture: joi.string().required(),
+    username: joi.string().min(3).max(20).required(),
+    picture: joi.string().uri().required(),
 });
 
 export default userSchema;
