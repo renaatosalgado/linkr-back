2852db89f3a9ba22938e9025930ea4c10ebe496f
augusto hansel <augustohansel@hotmail.com>
2022-04-01 08:39:24 -0300

    feat: search bar now orders by followed users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,4 +1,5 @@
 import { userRepository } from "../repositories/userRepository.js";
+import { followRepository } from '../repositories/followRepository.js';
 import bcrypt from "bcrypt";
 
 export async function createUser(req, res) {
@@ -23,13 +24,33 @@ export async function createUser(req, res) {
 
 export async function searchUser(req, res) {
   const { name } = req.query;
+  const { user } = res.locals;
   if (!name) return;
-
   try {
-    const result = await userRepository.getUserByName(name);
-
-    res.send(result.rows);
+    const {rows: usersFound} = await userRepository.getUserByName(name);
+    const {rows: followedUsers} = await followRepository.getFollows(user.id)
+    
+    usersFound.forEach((user, i) => {
+      followedUsers.forEach((followedUser) => {
+          if(followedUser.followedId === user.id){
+            usersFound[i] =  {...user, isFollowingLoggedUser: true}
+          }
+      })
+      if(!usersFound[i].isFollowingLoggedUser){
+        usersFound[i] =  {...user, isFollowingLoggedUser: false}
+      }
+  })
+    usersFound.sort((arr)=> orderByFollow(arr))
+    res.send(usersFound);
   } catch (error) {
     res.status(500).send(error);
+  }
+}
+
+function orderByFollow(arr) {
+  if(arr.isFollowingLoggedUser === true){
+    return -1
+  }else{
+    return 1
   }
 }
